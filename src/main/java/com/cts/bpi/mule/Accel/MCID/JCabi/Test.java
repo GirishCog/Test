c6d7b45dda94131f7c0993b447ae17a1e1faa4c4
Girish <girishreddy.b@gmail.com>
2015-07-13 11:36:32 -0400

    Publish
    Publish

---
+++ src/main/java/com/cts/bpi/mule/Accel/MCID/JCabi/Test.java
@@ -0,0 +1,17 @@
+package com.cts.bpi.mule.Accel.MCID.JCabi;
+
+import com.jcabi.github.Github;
+import com.jcabi.github.RtGithub;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	    Github github = new RtGithub("girishreddyB", "radhakrishna@123");
+	    
+
+
+	}
+
+}
